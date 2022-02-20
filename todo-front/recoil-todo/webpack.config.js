083fddb4a61f183014c4eac1d07fe04740c67bc8
ksh <iltk2000@naver.com>
2022-02-20 22:39:46 +0900

    feat: recoil project webpack init

---
+++ todo-front/recoil-todo/webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    open: true,
+    compress: true,
+    port: 4000
+  },
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[hash].js'
+  }
+};
